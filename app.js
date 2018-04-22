07737f117838c4893c4fb2be862f5cf751a6ef0c
Kyle Chin <kylechin@errorcodefive.com>
2018-04-22 09:46:20 -0400

    added config npm

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const http = require('http');
 
+//config
+const config = require('config');
+
 //express
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const port = 3000;
+const port = config.get('port');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, ()=> console.log('Example app Listening on port 3000!'))
+app.listen(port, ()=> console.log('Example app Listening on port 3000!'));
