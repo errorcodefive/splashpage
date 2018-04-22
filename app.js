6554dab226b028993112fd58f176847b15ef618c
Kyle Chin <kylechin@errorcodefive.com>
2018-04-22 10:22:54 -0400

    added ejs and changed view file structure

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const app = express();
 
 const port = config.get('port');
 
+//using ejs for templating
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, ()=> console.log('Example app Listening on port 3000!'));
