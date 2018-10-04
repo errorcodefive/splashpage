8cad6976e8152db6ff20fecf411d3ab3513e8ad1
Kyle Chin <errorcodefive@gmail.com>
2018-10-04 12:56:18 -0400

    Moved POST and UPDATE bookmarks to route file

--- routes/Bookmark.js
+++ routes/Bookmark.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Bookmark = require('../schemas/bookmarks');
 
 function getBookmarks(req,res){
+    console.log("Received GET Bookmark request");
     let query = Bookmark.find({});
     query.exec((err,bookmarks)=>{
         if(err) res.send(err);
@@ -9,4 +10,38 @@ function getBookmarks(req,res){
     });
 }
 
-module.exports = {getBookmarks};
+function postBookmark(req, res){
+    console.log("Received POST bookmark request");
+    var newBookmark = new Bookmark(
+        {
+            name: req.body.name,
+            link: req.body.link
+        }
+    );
+
+    newBookmark.save((err,newbm)=>{
+        if(err) res.send(err);
+        else{
+            res.json({success: true});
+        }
+    });
+}
+
+function updateBookmark(req, res){
+    console.log("Received PUT bookmark request");
+    var updateID = {_id: req.body._id};
+    var updateValues = {
+        name: req.body.name,
+        link: req.body.link,
+        command: req.body.command,
+        query_url: req.body.query_url
+    };
+    Bookmark.findById(updateID, (err, updateBookmark)=>{
+        if(err) res.send(err);
+        Object.assign(updateBookmark, updateValues).save((err, updateBookmark)=>{
+            if(err) res.send(err);
+            res.json({success: true, updateBookmark});
+        });
+    });
+}
+module.exports = {getBookmarks, postBookmark};
