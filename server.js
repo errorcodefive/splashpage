4167233eb8e6eb3830f22ab9eeb956d364574bf1
Kyle Chin <errorcodefive@gmail.com>
2018-10-31 15:34:22 -0400

    Listening on the port provided by heroku or 3000

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.route("/api/bookmarks/:id")
 	.put(Bookmark.updateBookmark)
 	.delete(Bookmark.deleteBookmark);
 
-module.exports = app.listen(8080, function(){
-	console.log('App started on port 8080');
+module.exports = app.listen(process.env.PORT || 3000, function(){
+	console.log('App started on port:'+process.env.port +'or 3000');
 });
