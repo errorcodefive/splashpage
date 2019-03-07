4f0445c2afd649fb79334a228b8fd2c522fac2ce
Kyle Chin <errorcodefive@gmail.com>
2019-03-07 11:38:31 -0500

    Creating Schema for RSS Feeds

---
+++ schemas/rssfeeds.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var rssfeedSchema = new Schema({
+    name: {type: String, required: true},
+    link: {type: String, required: true}
+});
+
+module.exports = mongoose.model('RSSFeed', rssfeedSchema);
